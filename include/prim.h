91b4061b1733ad3176e31f930fc1b5365cfc5b13
Sendhilishaan <ishaansendhil@gmail.com>
2026-06-17 17:13:36 +1000

    feat: add tanh prims

--- include/prim.h
+++ include/prim.h
@@ -7,4 +7,12 @@ void matmul_forward(const double* A, const double* B, double* out, int n, int k,
 
 void matmul_backward(const double* A, const double* B, const double* grad_out, double* out, int n, int k, int m);
 
+void matadd_broadcast_forward(const double* A, const double* B, int n, int m);
+
+void matadd_broadcast_backward(const double* grad_out, double* dX, double* db, int n, int m);
+
+void tanh_forward(const double* Z, double* out, int n, int m);
+
+void tanh_backward(const double* out, const double* grad_out, double* dz, int n, int m);
+
 #endif
--- src/ops/prim.c
+++ src/ops/prim.c
@@ -1,4 +1,6 @@
 #include "prim.h"
+#include <string.h>
+#include <math.h>
 /*
     forward and backward primitive kernels, naive implementations first
 
@@ -51,6 +53,57 @@ void matmul_backward(const double* A, const double* B, const double* grad_out, d
             for (int l = 0; l < n; l++) {
                 AT(db, i, j, m) += AT(A, l, i, k) * AT(grad_out, l, j, m); 
             }
+        }
+    }
+}
+
+/*
+add with broadcast down cols, row is one sample in batch
+
+A: (n, m) B: (1, m)
+*/
+void matadd_broadcast_forward(double* A, const double* B, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            AT(A, i, j, m) += AT(B, 0, j, m);
+        }
+    }
+}
+
+/*
+db: (1, m) cw sum should be 0 init
+*/
+void matadd_broadcast_backward(const double* grad_out, double* dX, double* db, int n, int m) {
+    // copy becayuse add is identity
+    memcpy(dX, grad_out, sizeof(double) * n * m);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            AT(db, 0, j, m) += AT(grad_out, i, j, m);
+        }
+    }
+}
+
+// sub
+
+/*
+tanh activation
+
+backward takes forward output buffer as a input
+*/
+
+void tanh_forward(const double* Z, double* out, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            AT(out, i, j, m) = tanh(AT(Z, i, j, m));
+        }
+    }
+}
+
+void tanh_backward(const double* out, const double* grad_out, double* dz, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            AT(dz, i, j, m) = AT(grad_out, i, j, m) * (1 - (AT(out, i, j, m) * AT(out, i, j, m)));
         }
     }
 }

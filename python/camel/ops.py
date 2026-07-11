2f52ba21aa0701e7b2a22e300c31a9afb29f76f2
Sendhilishaan <ishaansendhil@gmail.com>
2026-07-11 19:01:30 +1000

    feat: vector buffer class

--- python/camel/ops.py
+++ python/camel/ops.py
@@ -1,49 +1,42 @@
+from __future__ import annotations
 from ._c import c
 from ctypes import POINTER, c_double
 import numpy as np
 from typing import Tuple
 
-def as_ptr(arr: np.ndarray):
-    # np array to a ctypes pointer
-    return arr.ctypes.data_as(POINTER(c_double))
+class Vbuf:
+    def __init__(self, data: np.ndarray):
+        self.data = np.ascontiguousarray(data, np.float64)
+        self.ptr = self.data.ctypes.data_as(POINTER(c_double))
+        self.shape = self.data.shape
+    
+    @staticmethod # factory
+    def zeros(n: int, m: int) -> Vbuf:
+        return Vbuf(np.zeros(shape=(n, m)))
 
-class Primitives:
+
+class Ops:
+    # wrapping c functions
     @staticmethod
-    def matmul_forward(a: np.ndarray, b: np.ndarray) -> np.ndarray:
-        n = a.shape[0]
-        k = a.shape[1]
-        m = b.shape[1]
-
-        if k != b.shape[0]:
+    def matmul_forward(A: Vbuf, B: Vbuf) -> Vbuf:
+        if A.shape[1] != B.shape[0]:
             raise AttributeError # deal errors later
 
-        A = as_ptr(a)
-        B = as_ptr(b)
-        result_buf = np.zeros(shape=(n, m), dtype=float)
-        result_ptr = as_ptr(result_buf)
+        result_buf = Vbuf.zeros(A.shape[0], B.shape[1])
 
-        c.matmul_forward(A, B, result_ptr, n, k, m)
+        c.matmul_forward(A.ptr, B.ptr, result_buf.ptr, A.shape[0], A.shape[1], B.shape[1])
 
         return result_buf
 
     @staticmethod
-    def matmul_backward(a: np.ndarray, b:np.ndarray, grad_out: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-        n = a.shape[0]
-        k = a.shape[1]
-        m = b.shape[1]
+    def matmul_backward(A: Vbuf, B: Vbuf, grad_out: Vbuf) -> tuple[Vbuf, Vbuf]:
 
-        if k != b.shape[0]:
+        if A.shape[1] != B.shape[0]:
             raise AttributeError
 
-        A = as_ptr(a)
-        B = as_ptr(b)
-        grad_out_ptr = as_ptr(grad_out)
+        dA_buf = Vbuf.zeros(A.shape[0], A.shape[1])
+        dB_buf = Vbuf.zeros(A.shape[1], B.shape[1])
 
-        dA_buf = np.zeros(shape=(n, k))
-        dA_ptr = as_ptr(dA_buf)
-        dB_buf = np.zeros(shape=(k, m))
-        dB_ptr = as_ptr(dB_buf)
-
-        c.matmul_backward(A, B, grad_out_ptr, dA_ptr, dB_ptr, n, k, m)
+        c.matmul_backward(A.ptr, B.ptr, grad_out.ptr, dA_buf.ptr, dB_buf.ptr, A.shape[0], A.shape[1], B.shape[1])
 
         return (dA_buf, dB_buf)

26093062c1a767eb82b781387fa6f82549e70df2
Sendhilishaan <ishaansendhil@gmail.com>
2026-07-17 19:09:39 +1000

    feat: momentum for SGD

--- python/camel/optim.py
+++ python/camel/optim.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 from camel.tensor import Tensor
-from typing import List
+from typing import Iterable
+import numpy as np
 
 class Optimiser:
-    def __init__(self, lr: float, tensors: List[Tensor]):
+    def __init__(self, tensors: Iterable[Tensor], lr: float):
         self.lr = lr
-        self.tensors = tensors
+        self.tensors = list(tensors)
 
     def zero_grad(self):
         for t in self.tensors:
@@ -15,6 +16,13 @@ class Optimiser:
         raise NotImplementedError
     
 class SGD(Optimiser):
+    def __init__(self, tensors: Iterable[Tensor], lr: float, momentum=0.0):
+        super().__init__(tensors, lr)
+        self.momentum = momentum
+        self.velocity = [np.zeros_like(t.buf.data) for t in self.tensors]
+    
     def step(self):
-        for t in self.tensors:
-            t.buf.data -= t.grad.data * self.lr
+        for t, v in zip(self.tensors, self.velocity):
+            v *= self.momentum
+            v += t.grad.data
+            t.buf.data -= v * self.lr

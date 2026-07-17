24a99b783524e51508cab13a427db363c52227af
Sendhilishaan <ishaansendhil@gmail.com>
2026-07-17 19:10:35 +1000

    feat/fix: momentum in example and fix toml

--- examples/train_sin.py
+++ examples/train_sin.py
@@ -5,7 +5,7 @@ from camel.tensor import Tensor
 from camel.nn import MLP
 from camel.optim import SGD
 
-# Example training loop: fit sin with a small MLP, full-batch SGD
+# example training loop: fit sin with a small MLP, full-batch SGD with momentum
 X = np.linspace(-2*np.pi, 2*np.pi, 1000).reshape(-1, 1)
 X = X / X.std()
 y = np.sin(X)
@@ -14,7 +14,7 @@ X_t = Tensor(Vbuf(X), requires_grad=False)
 y_true = Tensor(Vbuf(y), requires_grad=False)
 
 mlp = MLP(1, [32, 32, 32, 1])
-opt = SGD(mlp.parameters(), 0.01)
+opt = SGD(mlp.parameters(), 0.01, 0.9)
 
 for i in range(1000):
     y_pred = mlp(X_t)
